e2a8d637171320b945b83d8bdafa56175c0c268f
tzenishbek@gmail.com <tzenishbek@gmail.com>
2026-01-10 00:12:05 +0100

    frontend/npmtest

---
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -0,0 +1,4 @@
+export default function AdminPage() {
+    return <h1 className="text-2xl p-6">Admin Dashboard</h1>;
+  }
+  
---
+++ frontend/src/pages/staff/StaffPage.tsx
@@ -0,0 +1,4 @@
+export default function StaffPage() {
+    return <h1 className="text-2xl p-6">Staff Tasks</h1>;
+  }
+  
