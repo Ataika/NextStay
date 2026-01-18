8b06d3dd8207f1d87d79cb13dfa4e5ec60eddd2a
Атай Кенешбеков <ataika@MacBook-Air-Ataj.local>
2026-01-18 23:27:32 +0600

    feature/added [Infra] Setup Pre-commit Hooks for Auto-formatting (Black/Isort/Flake8)

---
+++ scripts/init-db.sql
@@ -0,0 +1,6 @@
+-- creating user for  Superset (Read-Only)
+CREATE USER superset_ro WITH PASSWORD 'superset_pass';
+GRANT CONNECT ON DATABASE nextstay_db TO superset_ro;
+
+-- After creating the dbt schema, we will give the permission to  SELECT
+-- We will integrate it in  Milestone 2
